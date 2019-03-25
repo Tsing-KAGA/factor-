693e02458063964c6f2f553bf7eb65252c378739
mqhqwe <36285759+mqhqwe@users.noreply.github.com>
2019-03-25 23:32:41 +0800

    Add files via upload

---
+++ maqiuhao1.py
@@ -0,0 +1,39 @@
+# coding=utf8
+__author__ = 'mqh'
+import time
+
+import numpy as np
+import pandas as pd
+from FactorModule.FactorBase import FactorBase
+from DataReaderModule.Constants import ALIAS_FIELDS as t
+
+class Factor(FactorBase):
+
+    def __init__(self):
+        super(Factor,self).__init__()
+        self.neutral = False
+        self.factorName = __name__.split('.')[-1]
+        self.needFields = [t.HIGH,t.CLOSE,t.LOW]  # 设置需要的字段
+
+    def factor_definition(self):
+        """
+        收集派发指标
+        :return:
+        """
+        s = time.time()
+        length=5;
+        needData = self.needData# 计算所需数据
+        high=needData[t.HIGH];close=needData[t.CLOSE];low=needData[t.LOW]
+        r1=(2*close-high-low)/(high-low);r2=self.calculator.Delay(r1,9)
+        factor=r2-r1
+
+        print('factor {0} done with {1} seconds'.format(self.factorName, time.time() - s))
+        return factor
+
+    def run_factor(self):
+        self.run()
+
+
+
+fct = Factor()
+fct.run_factor()
