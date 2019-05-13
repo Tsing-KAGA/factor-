be08f53c615fa7de3bab94d144491f1f138baed5
mqhqwe <36285759+mqhqwe@users.noreply.github.com>
2019-05-13 21:35:44 +0800

    Add files via upload

---
+++ week6/gentic4_5.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env Python
+# -*- coding:utf-8 -*-
+# author: mqh
+import time
+import numpy as np
+import pandas as pd
+import math
+from FactorModule.FactorBase import FactorBase
+from DataReaderModule.Constants import ALIAS_FIELDS as t
+from CalculatorModule.OperatorPool import *
+class Factor(FactorBase):
+	def __init__(self):
+		super(Factor, self).__init__()
+		self.neutral = True
+		self.factorName = __name__.split('.')[-1]
+		self.needFields = [t.OPEN,t.HIGH,t.AMOUNT,t.CLOSE,t.HIGH,t.AMOUNT,t.HIGH,t.AMOUNT,t.CLOSE,t.HIGH,t.AMOUNT,t.CLOSE,t.LOW,t.AMOUNT,t.HIGH,t.VOLUME,t.OPEN,t.AMOUNT,t.HIGH,t.LOW,t.AMOUNT,t.HIGH,t.VOLUME,t.OPEN]# 设置需要的字段
+	def factor_definition(self):
+		s = time.time()
+		needData = self.needData  # 计算所需数据
+		factor=minus(rank(decaylinear7(tsmin5(standard(max7(needData[t.OPEN]))))), delay8(add(decaylinear10(corr30(regalpha60(tsmax5(needData[t.HIGH]), mean5(needData[t.AMOUNT])), diff10(regbeta10(needData[t.CLOSE], add(decaylinear10(corr30(regalpha60(tsmax5(needData[t.HIGH]), mean5(needData[t.AMOUNT])), decaylinear10(corr30(regalpha60(tsmax5(needData[t.HIGH]), mean5(needData[t.AMOUNT])), diff10(regbeta10(needData[t.CLOSE], add(decaylinear10(corr30(regalpha60(tsmax5(needData[t.HIGH]), mean5(needData[t.AMOUNT])), diff10(regbeta10(needData[t.CLOSE], needData[t.LOW])))), corr60(mean60(corr60(needData[t.AMOUNT], needData[t.HIGH])), log10(corr10(needData[t.VOLUME], needData[t.OPEN])))))))))), corr60(mean60(corr60(needData[t.AMOUNT], needData[t.HIGH])), log10(needData[t.LOW]))))))), corr60(mean60(corr60(needData[t.AMOUNT], needData[t.HIGH])), log10(corr10(needData[t.VOLUME], needData[t.OPEN]))))))
+		print('factor {0} done with {1} seconds'.format(self.factorName, time.time() - s))
+		return factor
+	def run_factor(self):
+		self.run()
+fct = Factor()
+fct.run_factor()
