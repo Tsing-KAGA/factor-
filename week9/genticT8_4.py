a96c190ef029a24e6114845d4ea605f30adb3010
mqhqwe <36285759+mqhqwe@users.noreply.github.com>
2019-05-16 22:30:57 +0800

    Add files via upload

---
+++ week9/genticT8_4.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env Python
+# -*- coding:utf-8 -*-
+# author: mqh
+import time
+import numpy as np
+import pandas as pd
+import math
+from FactorModule.FactorBase import FactorBase
+from DataReaderModule.Constants import ALIAS_FIELDS as t
+from CalculatorModule.OperatorPool import *
+class Factor(FactorBase):
+	def __init__(self):
+		super(Factor, self).__init__()
+		self.neutral = True
+		self.factorName = __name__.split('.')[-1]
+		self.needFields = list(set([t.AMOUNT,t.HIGH]))# 设置需要的字段
+	def factor_definition(self):
+		s = time.time()
+		needData = self.needData  # 计算所需数据
+		factor=sum7(diff9(corr20(mean10(needData[t.AMOUNT]), mean10(needData[t.HIGH]))))/7
+		print('factor {0} done with {1} seconds'.format(self.factorName, time.time() - s))
+		return factor
+	def run_factor(self):
+		self.run()
+fct = Factor()
+fct.run_factor()
