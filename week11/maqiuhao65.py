577cee4339db7d7928f342b21dc2b83be5d010c6
mqhqwe <36285759+mqhqwe@users.noreply.github.com>
2019-05-29 20:37:59 +0800

    Add files via upload

---
+++ week11/maqiuhao65.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env Python
+# -*- coding:utf-8 -*-
+# author: mqh
+import time
+import numpy as np
+import pandas as pd
+import math
+from FactorModule.FactorBase import FactorBase
+from DataReaderModule.Constants import ALIAS_FIELDS as t
+from CalculatorModule.OperatorPool import *
+class Factor(FactorBase):
+	def __init__(self):
+		super(Factor, self).__init__()
+		self.neutral = True
+		self.factorName = __name__.split('.')[-1]
+		self.needFields = list(set([t.HIGH,t.ADJOPEN,t.ADJOPEN,t.ADJOPEN,t.ADJOPEN,t.ADJOPEN,t.AMOUNT,t.ADJOPEN,t.ADJOPEN]))# 设置需要的字段
+	def factor_definition(self):
+		s = time.time()
+		needData = self.needData  # 计算所需数据
+		factor=sum8(std5(divide(diff10(sum10(log10(tsmin5(diff10(delay10(divide(diff10(sum10(log10(tsmin5(diff10(delay10(needData[t.HIGH])))))), regbeta15(diff10(var5(std5(std5(divide(diff10(sum10(multiply(needData[t.ADJOPEN], needData[t.ADJOPEN]))), needData[t.ADJOPEN]))))), mean15(multiply(needData[t.ADJOPEN], needData[t.ADJOPEN])))))))))), regbeta15(needData[t.AMOUNT], mean15(multiply(needData[t.ADJOPEN], needData[t.ADJOPEN]))))))
+		print('factor {0} done with {1} seconds'.format(self.factorName, time.time() - s))
+		return factor
+	def run_factor(self):
+		self.run()
+fct = Factor()
+fct.run_factor()
